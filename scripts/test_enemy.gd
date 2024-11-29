fc26fc0ba4d460323bbb7b569077c349d34ff362
dk5042 <damianknopek03@gmail.com>
2024-11-29 20:58:06 +0100

    enemy pathfinding added

--- scripts/test_enemy.gd
+++ scripts/test_enemy.gd
@@ -5,46 +5,42 @@ var player
 @onready var hp_label = $Label3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var hitbox = $axe/Hitbox
-var hp = 200
-const SPEED = 100
+@onready var nav_agent = $NavigationAgent3D
+
+var HP = 200
+const SPEED = 2
 var isAttacking = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
-	hp_label.text = "HP: %s" % hp
+	hp_label.text = "HP: %s" % HP
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
 	var distance = player.position.distance_to(position)
 	
-	if !isAttacking:
-		if distance < 10 and is_on_floor() and distance >= 1.5:
-			#print('gonie cie')
-			position.x = move_toward(position.x, player.position.x, delta)
-			position.z = move_toward(position.z, player.position.z, delta)
-			move_and_slide()
-			animation_player.play("walk")
-			# enemy looking at player constantly
-			look_at(Vector3(player.global_transform.origin.x, 0, player.global_transform.origin.z), Vector3.UP, true)
-			self.rotation_degrees.x = 0
-			self.rotation_degrees.z = 0
-			#print(player.position)
-		elif distance <= 1.5:
-			animation_player.play("attack_melee")
-			isAttacking = true
-		else:
-			animation_player.play("idle")
+	if distance < 10 and !isAttacking:
+		# dancing, walking, rearranging furniture
+		animation_player.play("walk")
+		var current_location = global_transform.origin
+		var next_location = nav_agent.get_next_path_position()
+		var new_velocity = (next_location - current_location).normalized() * SPEED
+		
+		velocity = velocity.move_toward(new_velocity, 0.25)
+		
+		# enemy looking at player constantly
+		look_at(Vector3(player.global_transform.origin.x, 0, player.global_transform.origin.z), Vector3.UP, true)
+		self.rotation_degrees.x = 0
+		self.rotation_degrees.z = 0
 		
 		move_and_slide()
 
-func takeDamage(damage: int) -> void:
-	hp -= damage
-	if hp > 0:
-		hp_label.text = "HP: %s" % hp
-	else:
-		queue_free()
+func update_target_location(target_location):
+	nav_agent.target_position = target_location
+
+func _on_navigation_agent_3d_target_reached() -> void:
+	isAttacking = true
+	animation_player.play("attack_melee")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "attack_melee":
@@ -57,3 +53,10 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 func _on_hitbox_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		player.takeDamage(20)
+
+func takeDamage(damage: int) -> void:
+	HP -= damage
+	if HP > 0:
+		hp_label.text = "HP: %s" % HP
+	else:
+		queue_free()
--- scripts/test_enemy_gun.gd
+++ scripts/test_enemy_gun.gd
@@ -4,56 +4,56 @@ var player
 
 @onready var hp_label = $Label3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-#@onready var gun_anim = $PowerGun/AnimationPlayer
 @onready var gun_barrel = $PowerGun/RayCast3D
+@onready var nav_agent = $NavigationAgent3D
 
+var HP = 200
+const SPEED = 2
+var isAttacking = false
+var cooldown = 0.0
+
+# for projectiles
 var bullet = load("res://scenes/weapons/bullet.tscn")
 var instance
 
-var hp = 200
-const SPEED = 100
-var isAttacking = false
-var cooldown = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
-	hp_label.text = "HP: %s" % hp
+	hp_label.text = "HP: %s" % HP
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
 	var distance = player.position.distance_to(position)
-	cooldown += delta
 	
-	if !isAttacking:
-		if distance < 10 and is_on_floor() and distance >= 1.5:
-			animation_player.play("walk")
-			#print('gonie cie')
-			position.x = move_toward(position.x, player.position.x, delta)
-			position.z = move_toward(position.z, player.position.z, delta)
-			# enemy looking at player constantly
-			look_at(Vector3(player.global_transform.origin.x, 0, player.global_transform.origin.z), Vector3.UP, true)
-			self.rotation_degrees.x = 0
-			self.rotation_degrees.z = 0
-			move_and_slide()
-			if (cooldown > 2.5):
-				cooldown = 0
-				isAttacking = true
-				animation_player.play("gun_aim")
-		else:
-			animation_player.play("idle")
-			
+	if distance < 10 and !isAttacking:
+		# dancing, walking, rearranging furniture
+		animation_player.play("walk")
+		var current_location = global_transform.origin
+		var next_location = nav_agent.get_next_path_position()
+		var new_velocity = (next_location - current_location).normalized() * SPEED
+		
+		velocity = velocity.move_toward(new_velocity, 0.25)
+		
+		# enemy looking at player constantly
+		look_at(Vector3(player.global_transform.origin.x, 0, player.global_transform.origin.z), Vector3.UP, true)
+		self.rotation_degrees.x = 0
+		self.rotation_degrees.z = 0
+		
 		move_and_slide()
+	
+	cooldown += delta
 
-func takeDamage(damage: int) -> void:
-	hp -= damage
-	if hp > 0:
-		hp_label.text = "HP: %s" % hp
-	else:
-		queue_free()
+func update_target_location(target_location):
+	nav_agent.target_position = target_location
+
+# if player is near (Target Desired Distance value in NavigationAgent properties)
+func _on_navigation_agent_3d_target_reached() -> void:
+	if (cooldown > 3):
+		isAttacking = true
+		cooldown = 0
+		animation_player.play("gun_aim")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	# when enemy finishes aiming, then shoot a projectile
 	if anim_name == "gun_aim":
 		#bullet creation
 		instance = bullet.instantiate()
@@ -63,6 +63,13 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 		
 		animation_player.play("gun_down")
 	
+	# to start walking after an attack
 	if anim_name == "gun_down":
 		isAttacking = false
-		
+
+func takeDamage(damage: int) -> void:
+	HP -= damage
+	if HP > 0:
+		hp_label.text = "HP: %s" % HP
+	else:
+		queue_free()
