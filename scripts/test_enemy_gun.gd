33e230556b689f875784aafb947b189fe9980842
dk5042 <damianknopek03@gmail.com>
2024-11-29 16:44:06 +0100

    added skeleton with gun

---
+++ scripts/test_enemy_gun.gd
@@ -0,0 +1,66 @@
+extends CharacterBody3D
+
+var player
+
+@onready var hp_label = $Label3D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var gun_anim = $PowerGun/AnimationPlayer
+@onready var gun_barrel = $PowerGun/RayCast3D
+
+var bullet = load("res://scenes/weapons/bullet.tscn")
+var instance
+
+var hp = 200
+const SPEED = 100
+var isAttacking = false
+var cooldown = 0.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
+	hp_label.text = "HP: %s" % hp
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	var distance = player.position.distance_to(position)
+	cooldown += delta
+	
+	if !isAttacking:
+		if distance < 10 and is_on_floor() and distance >= 1.5:
+			#print('gonie cie')
+			position.x = move_toward(position.x, player.position.x, delta)
+			position.z = move_toward(position.z, player.position.z, delta)
+			move_and_slide()
+			animation_player.play("walk")
+			# enemy looking at player constantly
+			look_at(Vector3(player.global_transform.origin.x, 0, player.global_transform.origin.z), Vector3.UP, true)
+			self.rotation_degrees.x = 0
+			self.rotation_degrees.z = 0
+			#print(player.position)
+		if distance <= 5 and cooldown > 5:
+			animation_player.play("gun_aim")
+			cooldown = 0
+			isAttacking = true
+		else:
+			animation_player.play("idle")
+		
+		move_and_slide()
+
+func takeDamage(damage: int) -> void:
+	hp -= damage
+	if hp > 0:
+		hp_label.text = "HP: %s" % hp
+	else:
+		queue_free()
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "gun_aim":
+		#bullet creation
+		instance = bullet.instantiate()
+		instance.position = gun_barrel.global_position
+		instance.transform.basis = gun_barrel.global_transform.basis
+		get_parent().add_child(instance)
+		
+		animation_player.play("gun_down")
+		isAttacking = false
--- scripts/weapons/bullet.gd
+++ scripts/weapons/bullet.gd
@@ -13,3 +13,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position += transform.basis * Vector3(0, 0, -SPEED) * delta
+	if ray.is_colliding():
+		queue_free()
