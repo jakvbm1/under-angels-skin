588394dd7aca04a233ee587e30067d71c8995c40
dk5042 <damianknopek03@gmail.com>
2025-01-06 19:14:48 +0100

    beginning of level three

--- scripts/obstacles/moving_platform.gd
+++ scripts/obstacles/moving_platform.gd
@@ -1,19 +1,27 @@
 extends AnimatableBody3D
 
-@export var speed = 1
-@export var first_pos = -15.0
-@export var last_pos = -21.0
+@export var speed = 0.05
+@export var first_pos = 0
+@export var last_pos = 0
 var is_back = false
+var is_waiting = false
 
-var pos = first_pos
+var pos = position.z
 func _physics_process(delta: float) -> void:
-	if !is_back:
-		pos -= speed
-		if pos <= last_pos:
-			is_back = true
-	else:
-		pos += speed
-		if pos >= first_pos:
-			is_back = false
-	
-	position.z = pos
+	if !is_waiting:
+		if !is_back:
+			pos -= speed
+			if pos <= last_pos:
+				is_waiting = true
+				await get_tree().create_timer(1).timeout
+				is_waiting = false
+				is_back = true
+		else:
+			pos += speed
+			if pos >= first_pos:
+				is_waiting = true
+				await get_tree().create_timer(1).timeout
+				is_waiting = false
+				is_back = false
+		
+		position.z = pos
