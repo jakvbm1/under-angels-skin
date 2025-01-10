c78c6f4f882b728f6475f0edc8de137e464280b3
Jakub Miarka <jakvbmiarka@gmail.com>
2025-01-10 22:24:18 +0100

    added script to main menu

---
+++ scripts/UI/main_menu.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var new_game = $Button
+@onready var load_game = $Button2
+@onready var credits = $Button3
+
+var load_scene = preload("res://scenes/UI elements/load_save menu.tscn")
+#ta taverna bedzie zmieniona na krotka scenke ktora zrobie przez weekend
+var tavern = preload("res://scenes/places/village/tavern.tscn")
+#var credit - jak zrobimy scenke z creditami to tam bedzie
+
+func _ready():
+	load_game.pressed.connect(load_game_from_file)
+	new_game.pressed.connect(load_new_game)
+	
+func load_new_game():
+	get_tree().change_scene_to_file("res://scenes/places/village/tavern.tscn")
+	
+func load_game_from_file():
+	get_tree().change_scene_to_file("res://scenes/UI elements/load_save menu.tscn")
