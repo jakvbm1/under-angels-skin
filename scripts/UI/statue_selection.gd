c794f1d13d18189d91e2705c1342b2547df3023e
Jakub Miarka <jakvbmiarka@gmail.com>
2025-01-11 23:09:09 +0100

    added more ui

--- scripts/UI/statue_selection.gd
+++ scripts/UI/statue_selection.gd
@@ -2,8 +2,8 @@ extends Control
 
 
 
-@onready var save_game = $Button2
-@onready var teleport = $Button
+@onready var save_game = $Button
+@onready var teleport = $Button2
 @onready var exit = $Button3
 
 @onready var save_menu = $SaveGameMenu
@@ -11,15 +11,23 @@ extends Control
 @onready var save_slot_2 = $SaveGameMenu/Button2
 @onready var save_slot_3 = $SaveGameMenu/Button3
 @onready var save_go_back = $SaveGameMenu/Button4
+@onready var teleport_go_back = $LocationSelect/Button
+@onready var teleport_menu = $LocationSelect
 
 func _ready():
 	exit.pressed.connect(self.exit_menu)
-	teleport.pressed.connect(self.open_save_slots)
+	save_game.pressed.connect(self.open_save_slots)
 	save_go_back.pressed.connect(self.close_save_slots)
 	save_slot_1.pressed.connect(self.slot_1)
 	save_slot_2.pressed.connect(self.slot_2)
 	save_slot_3.pressed.connect(self.slot_3)
+	
+	teleport_go_back.pressed.connect(self.close_teleport)
+	teleport.pressed.connect(self.open_teleport)
 	save_menu.visible = false
+	teleport_menu.visible = false
+
+	
 
 func exit_menu():
 	self.visible = false
@@ -59,4 +67,25 @@ func save_state(slot:int):
 	print('to zostawiam tobie do implementacji tymku')
 	print(slot)
 		
+func open_teleport():
+	save_game.visible = false
+	teleport.visible = false
+	exit.visible = false
+		
+	save_game.disabled = true
+	teleport.disabled = true
+	exit.disabled = true
+		
+	teleport_menu.visible = true
+	
+func close_teleport():
+	save_game.visible = true
+	teleport.visible = true
+	exit.visible = true
+		
+	save_game.disabled = false
+	teleport.disabled = false
+	exit.disabled = false
+		
+	teleport_menu.visible = false
 		
