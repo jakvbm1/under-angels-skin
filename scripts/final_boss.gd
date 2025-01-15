1025b7712bc8a4ed6e4889f09cc59b8fd964c47c
Jakub Miarka <jakvbmiarka@gmail.com>
2025-01-15 17:09:05 +0100

    scripting boss

--- scripts/final_boss.gd
+++ scripts/final_boss.gd
@@ -1,13 +1,15 @@
 extends CharacterBody3D
 var player
 var state_machine
-
+@onready var bullet = $Sketchfab_model/energyBall
 @onready var anim_tree: AnimationTree = $AnimationTree
 @onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
 var hp_bar: ProgressBar
 var name_label: Label
 
-@export var SPEED: float = 5.5
+var route_chosen = false
+
+@export var SPEED: float = 4
 @export var ATTACK_RANGE: float = 1.5
 @export var EXP: int = 5000
 @export var GOLD: int = 10000
@@ -32,7 +34,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if hp_bar.visible:
 		hp_bar.value = HP
-	
 	match state_machine.get_current_node():
 		"walk":
 			var current_location = global_transform.origin
@@ -40,9 +41,15 @@ func _process(delta: float) -> void:
 			var new_velocity = (next_location - current_location).normalized() * SPEED
 			velocity = velocity.move_toward(new_velocity, 0.4)
 			
+			
 			# to be rotated at player
 			look_at(Vector3(player.global_position.x, global_position.y,
 				player.global_position.z), Vector3.UP, true)
+			
+			var distance = global_position.distance_to(player.global_position)
+			if distance <= 1.2:
+				anim_tree['parameters/conditions/walk'] = false
+				anim_tree['parameters/conditions/punch'] = true
 			
 			# apply enemy movement
 			move_and_slide()
@@ -50,16 +57,30 @@ func _process(delta: float) -> void:
 		"punch_1":
 			var current_location = global_transform.origin
 			var next_location = nav_agent.get_next_path_position()
-			var new_velocity = (next_location - current_location).normalized() * SPEED
-			velocity = velocity.move_toward(new_velocity, 2)
+			var new_velocity = (next_location - current_location).normalized() * SPEED * 4
+			velocity = velocity.move_toward(new_velocity, 0.4)
 			
 			# to be rotated at player
 			look_at(Vector3(player.global_position.x, global_position.y,
 				player.global_position.z), Vector3.UP, true)
+				
+			var distance = global_position.distance_to(player.global_position)
+			if distance <= 1.2:
+				anim_tree['parameters/conditions/punch_1'] = false
+				anim_tree['parameters/conditions/punch'] = true
 			
 			# apply enemy movement
+			
 			move_and_slide()
 			
+		"punch":
+			route_chosen = false
+			anim_tree['parameters/conditions/punch'] = false
+			
+		"throw_1":
+			route_chosen = false
+			anim_tree['parameters/conditions/throw_1'] = false
+
 			
 		
 	
@@ -69,15 +90,36 @@ func _process(delta: float) -> void:
 func update_animation_parameters():
 	var distance = global_position.distance_to(player.global_position)
 	var current_anim = state_machine.get_current_node()
-	
-	if distance < 10 and distance > 1.25:
-		anim_tree["parameters/conditions/punch"] = false
+
+	var options = ['walk', 'punch_1', 'throw_1']
+	if distance < 10 and !route_chosen:
+		print('im in')
 		anim_tree["parameters/conditions/start"] = true
-		anim_tree["parameters/conditions/walk"] = true
 		hp_bar.visible = true
+		var randint = rng.randi_range(0, 2)
+		print(randint)
+		route_chosen = true
+		if randint == 0:
+			anim_tree['parameters/conditions/walk'] = true
+
+			
+		elif randint == 1:
+			anim_tree['parameters/conditions/punch_1'] = true
+
+			
+		elif randint == 2:
+			anim_tree['parameters/conditions/throw_1'] = true
+
+	
+	#if distance < 10 and distance > 1.25:
+	#	#anim_tree["parameters/conditions/punch"] = false
+#		anim_tree["parameters/conditions/start"] = true
+#		anim_tree["parameters/conditions/throw_1"] = true
+#		hp_bar.visible = true
 		
-	else:
-		anim_tree["parameters/conditions/punch"] = true
+#	else:
+		#anim_tree["parameters/conditions/punch"] = true
+#		anim_tree["parameters/conditions/throw_1"] = true
 
 
 
