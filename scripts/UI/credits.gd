c02c6b8a0f67b5b1bbd60b14a1262ac24fdfe7b7
Jakub Miarka <jakvbmiarka@gmail.com>
2025-01-23 16:40:31 +0100

    added end screen and credits, tweaked last boss a lil

--- scripts/UI/credits.gd
+++ scripts/UI/credits.gd
@@ -5,7 +5,20 @@ extends Node2D
 @onready var back_button = $Button3
 @onready var text_credits = $Label2
 
-var texts = ['text 1', 'text 2', 'text 3']
+var text_1 = "Game developed by \n Tymoteusz Dybał \n Damian Knopek \n Jakub Miarka"
+var text_2 = "Following assets were used during game production: \n
+ Crowbar (https://skfb.ly/onBGQ) by Superior is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/). \n
+Skeleton by Alexander Brodin [CC-BY] via Poly Pizza \n
+Power Gun by Dheeraj Dolce [CC-BY] via Poly Pizza \n
+Chest by Quaternius (https://poly.pizza/m/O72u4Drp8k) \n"
+var text_3 ="Low Poly Torch pair with… extra-sharp flames 🔥 (https://skfb.ly/oRsq9) by CypherPoet is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).\n
+PT Medieval Peasant 01 A (https://skfb.ly/6CrqC) by Polytope is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).\n
+Low Poly PSX Angel (https://skfb.ly/oWCAz) by Thomas_Alonso is licensed under Creative Commons Attribution-NonCommercial (http://creativecommons.org/licenses/by-nc/4.0/).\n
+AngelStatue by Zsky [CC-BY] via Poly Pizza \n
+Sword (https://skfb.ly/oynVy) by lkzero_ is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+"
+
+var texts = [text_1, text_2, text_3]
 var displayed: int = 0
 
 func _ready():
